63b4a8927c9766d4c7bece87d55afd334758298e
Gustazinalb <gustazin.2501.albuquerque@gmail.com>
2026-09-10 20:50:44 -0300

    teste

--- imdb/admin/index.php
+++ imdb/admin/index.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel Administrativo</title>
 
-    <base href="http://localhost/2ads/imdb/admin/">
+    <base href="http://localhost/desafio_burnes/admin/">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link href="css/summernote-bs5.min.css"
